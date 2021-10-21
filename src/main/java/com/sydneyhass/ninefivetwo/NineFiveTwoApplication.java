5a0f65edbc42d5e2987d35daf58e4cbf8d653a82
Sydney <sydney.kiersten@gmail.com>
2021-10-20 21:08:32 -0400

    Initial commit

---
+++ src/main/java/com/sydneyhass/ninefivetwo/NineFiveTwoApplication.java
@@ -0,0 +1,13 @@
+package com.sydneyhass.ninefivetwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NineFiveTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NineFiveTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sydneyhass/ninefivetwo/NineFiveTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sydneyhass.ninefivetwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NineFiveTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
